d8568138c4c7b99fc6a13b910bdf7b82414ba821
Ryxune <40349078+Ryxune@users.noreply.github.com>
2019-10-07 17:09:47 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+axios.defaults.baseURL = "http://127.0.0.1/3000";
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrap">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
